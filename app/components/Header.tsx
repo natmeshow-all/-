021332ee3056716f5d0971b16c81d46b2bc31a43
natshow <natmeshow@gmail.com>
2026-07-21 08:25:49 +0700

    style: hide version number for non-admin users

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -135,7 +135,7 @@ export default function Header({ className = "" }: HeaderProps) {
                                                 {t("actionSignOut")}
                                             </button>
 
-                                            <VersionDisplay />
+                                            {isAdmin && <VersionDisplay />}
                                         </div>
                                     </>
                                 )}
